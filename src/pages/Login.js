e4569c97f8d1d75e32df11609ab47aaf7a9343e1
lishu <lishugupta652@gmail.com>
2022-03-01 10:20:42 +0530

    resolved issues

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import Navbar from "";
+import Navbar from "../components/Navbar";
+
 // const Navbar=styled.div`
 //  display: flex;
 //   justify-content: left;
@@ -9,77 +10,91 @@ import Navbar from "";
 //   width: 100%;
 //   flex-wrap:rows;
 //   `;
-  const LForm=styled.div`
-  display:flex;
-  justify-content:center;
-  align-items:center;
+const LForm = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 6rem;
-  .Main{
-    background-color: #EBEBEB;
+  .Main {
+    background-color: #ebebeb;
     color: black;
-    padding:5rem;
-    border:2px solid black;
-    border-radius:1px 25px;
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    padding: 5rem;
+    border: 2px solid black;
+    border-radius: 1px 25px;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   }
   .Main:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.5);
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.5);
   }
-  .hover:hover{
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  .hover:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   }
-  .submit{
-    background-color: #e7e7e7; 
+  .submit {
+    background-color: #e7e7e7;
     color: black;
-    font-size:16px;
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    font-size: 16px;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   }
-  .submitSign{
-    margin-left:18px;
+  .submitSign {
+    margin-left: 18px;
     background-color: #555555;
-    color:white;
-    font-size:16px;
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.1);
-  }  
-  `;
-  const Radbutt=styled.div`
+    color: white;
+    font-size: 16px;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.1);
+  }
+`;
+const Radbutt = styled.div`
   font-size: 1rem;
-    label{
-      margin-right 20px;
-    }
-  `;
+  label {
+    margin-right: 20px;
+  }
+`;
 
 const Login = (props) => {
   return (
-  <div >   
-    <Navbar/>
-    <hr />
-    
-   <LForm>
-    <div class="Main">
-      <div>
-      <h2>LOGIN</h2>
-    <br /><br /><br />
-    <label htmlFor="username">UserName  </label>
-    <input type="text" class="hover" name="username"/>
-    <br /><br />
-    <label htmlFor="password">Password  </label>
-    <input type="password" class="hover"name="password"/>
-      </div>
-      <br />
-      <Radbutt><input type="radio" id="MessOwner"  name="radioButt" value="Mess Owner"/>
-      <label  htmlFor="MessOwner">Mess Owner</label>
-      <input class="cust_name" type="radio" id="Customer"  name="radioButt" value="customer"/>
-      <label htmlFor="customer">Customer</label>    
-      </Radbutt><br />
-      <button class="submit" >Login</button>
-      <button class="submitSign">SignUp</button>
-     </div>    
-    </LForm>      
-      
-  </div>
-  
-    );
+    <div>
+      <Navbar />
+      <hr />
+
+      <LForm>
+        <div class="Main">
+          <div>
+            <h2>LOGIN</h2>
+            <br />
+            <br />
+            <br />
+            <label htmlFor="username">UserName </label>
+            <input type="text" class="hover" name="username" />
+            <br />
+            <br />
+            <label htmlFor="password">Password </label>
+            <input type="password" class="hover" name="password" />
+          </div>
+          <br />
+          <Radbutt>
+            <input
+              type="radio"
+              id="MessOwner"
+              name="radioButt"
+              value="Mess Owner"
+            />
+            <label htmlFor="MessOwner">Mess Owner</label>
+            <input
+              class="cust_name"
+              type="radio"
+              id="Customer"
+              name="radioButt"
+              value="customer"
+            />
+            <label htmlFor="customer">Customer</label>
+          </Radbutt>
+          <br />
+          <button class="submit">Login</button>
+          <button class="submitSign">SignUp</button>
+        </div>
+      </LForm>
+    </div>
+  );
 };
 
 export default Login;
